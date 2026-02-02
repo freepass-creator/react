1cd1caeb91ad5e376108f21e34c4a2f7e3c43232
freepass-creator <dudguq@gmail.com>
2026-02-02 16:57:01 +0900

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>■ 렌터카 시스템 리액트 테스트</h1>
+      <p>이 화면이 보인다면 리액트 폴더 구조가 완성된 것입니다.</p>
+    </div>
+  );
+}
+
+export default App;
