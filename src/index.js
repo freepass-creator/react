1deca3e11dfc6bdea693678a4f5e08abfe0cabca
freepass-creator <dudguq@gmail.com>
2026-02-02 18:20:35 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App'; // 사용자님의 App.js를 불러옴
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
